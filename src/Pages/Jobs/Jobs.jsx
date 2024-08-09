50707f2a029a633e880fabe94ef26d331207490b
Mohammed Owais <filledstackdeveloper@gmail.com>
2024-08-09 16:14:47 +0530

    changes

--- src/Pages/Jobs/Jobs.jsx
+++ src/Pages/Jobs/Jobs.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { GetApi } from "../utilis/Api_Calling";
 
+
 const Jobs = () => {
   const navigate = useNavigate();
   const [value, setValue] = useState(50);
@@ -18,7 +19,7 @@ const Jobs = () => {
   const GetAllJobs = async () => {
     try {
       const Getjobdata = await GetApi(`api/AdminRoutes/GetAllJobs`);
-      console.log(Getjobdata?.data);
+      console.log(Getjobdata?.data.data[1]);
       setAllJobs(Getjobdata?.data?.data);
       Settotaljob(Getjobdata?.data?.data?.length);
       setLoading(false);
@@ -122,15 +123,18 @@ const Jobs = () => {
           Loading...
         </div>
       ) : (
-        <div className="p-[15px] font-[Outfit] flex flex-col justify-start items-center w-full bg-[#f8f9fa]">
+        <div
+          className="flex flex-col justify-start items-center w-full bg-[#f8f9fa]"
+          style={{ fontFamily: "poppins" }}
+        >
           <>
-            <div className="w-full flex justify-start items-center">
-              <p className="text-[16px] font-[400] text-[#000] text-opacity-[50%] px-[34px]">
+            <div className="w-full flex justify-start items-center bg-[#f8f9fa] py-2">
+              <p className="text-[16px] font-[400] text-[#000] text-opacity-[50%] px-[34px] cursor-pointer">
                 Home &gt; Jobs
               </p>
             </div>
-            <div className="flex flex-col justify-start items-start w-full md:flex-row gap-[34px] mt-[16px] px-[34px]">
-              <div className="bg-white h-[883px] w-1/5 rounded-[8px]  flex flex-col  items-start p-[26px] border-[1px] ">
+            <div className="flex flex-col justify-start items-start w-full md:flex-row gap-[34px] mt-[16px] px-[34px] bg-[#f8f9fa]">
+              <div className="bg-white min-h-[100] w-1/5 rounded-[8px]  flex flex-col  items-start p-[26px] border-[1px] ">
                 <p className="text-md font-[400] text-gray-900">All Fillters</p>
                 <br />
                 <hr className="border-b w-full" />
@@ -215,9 +219,9 @@ const Jobs = () => {
                     Years of experience
                   </p>
                   <input
-                    className="border-[1px] h-[42px] border-[#d9d9d9] rounded-[4px]"
+                    className="border-[1px] w-3/4 h-[42px] border-[#d9d9d9] rounded-[4px]"
                     type="text"
-                    value={experienceFilter} // Connect value to the state
+                    value={experienceFilter}
                     onChange={(e) => setExperienceFilter(e.target.value)}
                   />
                 </div>
@@ -230,92 +234,96 @@ const Jobs = () => {
                   </p>
                 </div>
               </div>
-              <div className="flex gap-[30px] flex-col w-3/5">
+              <div className="flex gap-[10px] flex-col w-3/5 max-h-[80vh] overflow-scroll px-2 bg-[#f8f9fa]">
+                <div className="w-full flex justify-between items-center my-2 px-4">
+                  <span className="text-sm text-gray-600">
+                    {totaljob} Jobs Available
+                  </span>
+                  <span className="text-sm text-blue-600 cursor-pointer font-semibold">
+                    send me more jobs like these
+                  </span>
+                </div>
                 {applyFilters(AllJobs)?.length > 0 ? (
                   applyFilters(AllJobs)?.map((job, index) => {
                     const isJobApplied = appiledjobs.includes(job._id);
                     return (
                       <div
+                        onClick={() => {
+                          jobDetail(job?._id);
+                        }}
                         key={index}
-                        className="bg-white rounded-[16px] border-[1px] border-[#efecec] p-[15px]"
+                        className="bg-white rounded-3xl border-[1px] border-[#efecec] p-5"
                       >
                         <div className="mt-[8px]">
                           <div className="flex justify-between gap-[20px]">
-                            <p className="text-xl font-bold lg:w-[476px] flex-wrap">
+                            <p className="text-xl font-medium lg:w-[476px] flex-wrap">
                               {job?.positionName}
                             </p>
-                            <img
-                              src="/images/jobsIcon.svg"
-                              className="w-[60px] h-[60px]"
-                              alt=""
-                            />
-                          </div>
-                          <p className="text-gray-600 mt-[-2rem] text-md font-semibold">
-                            {job?.Company?.Name}
-                          </p>
-                          <div className="flex flex-col lg:flex-row text-[14px] font-[400] text-black text-opacity-[50%] mt-[22px] gap-[10px] lg:gap-[54px]">
-                            <div className="flex flex-col gap-[8px]">
-                              <div className="flex gap-[4px]">
-                                <img
-                                  src="/images/carbon_play-outline.svg"
-                                  alt=""
-                                />
-                                <p>START DATE</p>
-                              </div>
-                              <p>Immediately</p>
-                            </div>
-                            <div className="flex flex-col gap-[8px]">
-                              <div className="flex gap-[4px]">
-                                <img src="/images/ctcVector.svg" alt="" />
-                                <p>Salary</p>
-                              </div>
-                              {/* <p>Rs. {formatSalary(job?.maxSalary)}</p> */}
-                              <p>Rs. {job?.maxSalary}</p>
-                            </div>
-                            <div className="flex flex-col gap-[8px]">
-                              <div className="flex gap-[4px]">
-                                <img src="/images/bytesize_work.svg" alt="" />
-                                <p>EXPERIENCE</p>
-                              </div>
-                              <p>0-{job?.Experience} Years</p>
-                            </div>
-                          </div>
-                          <div className="flex mt-[28px] gap-[28px] border-b-[1px] border-[#ebe6e6] pb-[13px]">
-                            <div className="flex gap-[4px] bg-[#f1ffe5] rounded-[3px] px-[6px] py-[2px]">
-                              <img
-                                src="/images/pepicons-pencil_rewind-time.svg"
-                                className="w-[18px] h-[18px]"
-                                alt=""
-                              />
-                              <p>{NewformatDate(job?.createdAt)}</p>
-                            </div>
-                            <p className="text-[12px] font-[400] text-[#000] text-opacity-[50%] bg-[#d9d9d9] bg-opacity-[50%] p-[4px] ">
-                              {job?.contractDetails}
+                            <p>
+                              {isJobApplied ? (
+                                <button className="w-[103px] h-[32px] text-white bg-gradient-to-tl from-[#0f87b3] to-[#462da1] rounded-[5px] flex justify-center items-center">
+                                  Applied
+                                </button>
+                              ) : (
+                                <button
+                                  onClick={() => {
+                                    jobDetail(job?._id);
+                                  }}
+                                  className="w-[103px] h-[32px] text-white bg-gradient-to-tl from-[#0f87b3] to-[#462da1] rounded-[5px] flex justify-center items-center"
+                                >
+                                  Apply now
+                                </button>
+                              )}
                             </p>
                           </div>
-                          <div className="flex w-full pt-[18px] gap-[15px] justify-end items-center">
-                            <div
-                              onClick={() => {
-                                jobDetail(job?._id);
-                              }}
-                              className="border-[1px] flex justify-center items-center text-[#4234a2] text-[14px] font-[500] border-[#4234a2] rounded-[5px] w-[117px] h-[32px]"
-                            >
-                              View details
+                          <p className="text-gray-600  text-md font-normal">
+                            {job?.Company?.Name}
+                          </p>
+                          <div className="flex flex-col  text-[14px] font-[400] text-black text-opacity-[50%] mt-[5px] gap-2">
+                            <div className="flex justify-start items-center flex-wrap gap-3">
+                              <span>
+                                <i className="fa-solid fa-briefcase mr-2"></i>{" "}
+                                0-5 Years &nbsp; |
+                              </span>
+                              <span>
+                                <i className="fa-solid fa-dollar-sign mr-2"></i>{" "}
+                                {job?.minSalary}-{job?.maxSalary} salary &nbsp;
+                                |
+                              </span>
+                              <span>
+                                {" "}
+                                <i className="fa-solid fa-location-dot mr-2"></i>
+                                {job?.location} &nbsp; |
+                              </span>
                             </div>
-                            {isJobApplied ? (
-                              <button className="w-[103px] h-[32px] text-white bg-gradient-to-tl from-[#0f87b3] to-[#462da1] rounded-[5px] flex justify-center items-center">
-                                Applied
-                              </button>
-                            ) : (
-                              <button
-                                onClick={() => {
-                                  jobDetail(job?._id);
-                                }}
-                                className="w-[103px] h-[32px] text-white bg-gradient-to-tl from-[#0f87b3] to-[#462da1] rounded-[5px] flex justify-center items-center"
-                              >
-                                Apply now
-                              </button>
-                            )}
+                            <div className="flex justify-start items-center flex-wrap gap-3">
+                              <span>
+                                <i className="fa-solid fa-clipboard mr-2"></i>{" "}
+                                Must have
+                                {job?.skillAssessment?.map((skill) => (
+                                  <span className="mx-1">{skill.skill},</span>
+                                ))}
+                              </span>
+                            </div>
+                            <div className="flex justify-start items-center flex-wrap gap-3">
+                              {job?.skillsRequired?.map((skill) => (
+                                <span>{skill} &nbsp;&nbsp; .</span>
+                              ))}
+                            </div>
+                            <div className="w-full flex justify-between items-center flex-wrap gap-3">
+                              <span className="flex gap-3">
+                                <img
+                                  src="/images/pepicons-pencil_rewind-time.svg"
+                                  className="w-[18px]"
+                                  alt=""
+                                />{" "}
+                                {NewformatDate(job?.createdAt)}
+                              </span>
+                              <span className="cursor-pointer">
+                                <i className="fa-solid fa-bookmark mr-3"></i>{" "}
+                                Save
+                              </span>
+                            </div>
                           </div>
                         </div>
                       </div>
@@ -326,6 +334,9 @@ const Jobs = () => {
                     No Jobs Found With This query
                   </div>
                 )}
+              </div>
+              <div className="bg-white min-h-[100] w-1/5 rounded-[8px]  flex flex-col  items-start p-[26px] border-[1px] ">
+                
               </div>
             </div>
           </>
