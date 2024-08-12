5b0877412acb22caa32be43bbf2b2ea6e51ff587
owaisansarigit <filledstackdeveloper@gmail.com>
2024-08-12 08:01:16 +0530

    interview

--- src/Pages/Jobs/StartInterview.jsx
+++ src/Pages/Jobs/StartInterview.jsx
@@ -77,6 +77,7 @@ const StartInterview = () => {
     } else {
       if (status === "recording") {
         stopRecording();
+        handleSubmitVideo();
       } else {
         handleSubmitVideo();
       }
